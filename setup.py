3f885de701ca43d310f871f818cb0dd38438ed01
5f0 <96919731+5f0ne@users.noreply.github.com>
2024-01-25 17:31:43 +0100

    Template issues fixed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as r:
 
 setup(
     name="examinerview",             # This name is used in: pip install examinerview
-    version="1.0.0",
+    version="1.0.1",
     author="5f0",
     url="https://github.com/5f0ne/examinerview",
     description="Create visual timelines for forensic investigations",
